e18ac7bb5d3383b824e788476f5962be0360c76c
Grigory Vorobev <grishaMas@yandex.ru>
2024-12-20 19:24:14 +0500

    Initial commit

---
+++ src/scripts/text_typing.js
@@ -0,0 +1,16 @@
+class Typing {
+    constructor(element) {
+        this.element = element
+    }
+
+    text_typing(text, delay) {
+        
+    }
+}
+
+class TextTyping {
+    constructor(element, text_array) {
+        this.element = element
+        this.text_array = text_array
+    }
+}
